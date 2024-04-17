ab76a7bf9a1b89233f9a35e2b4d52c6b551e91b9
RIYA SUBHASH MATE <145019243+RiyaMate@users.noreply.github.com>
2024-04-17 19:32:28 -0400

    first commit

---
+++ client/src/config.js
@@ -0,0 +1,3 @@
+export const API = "http://localhost:8000/api";
+export const GOOGLE_PLACES_KEY = "AIzaSyCqKnXBxGBtxTnqZdmLCNrgi0YGpglJr2k";
+export const GOOGLE_MAPS_KEY = "AIzaSyCOyWv6Nua9uRl5Pp4fWFgi27Pl0Mu2ujk";
